a4a229ad6442bbf6e26ec7c8fac54191cad28bdb
Flier Lu <flier.lu@gmail.com>
2017-07-26 17:50:53 +0800

    remove unused functions

--- llvm-jit/src/lib.rs
+++ llvm-jit/src/lib.rs
@@ -47,5 +47,5 @@ pub mod ops {
     pub use builder::{AShr, Add, AggregateRet, And, Br, CondBr, ExactSDiv, ExactUDiv, FAdd, FDiv,
                       FMul, FNeg, FRem, FSub, LShr, Mul, NSWAdd, NSWMul, NSWNeg, NSWSub, NUWAdd,
                       NUWMul, NUWNeg, NUWSub, Neg, Not, Or, Ret, RetVoid, SDiv, SRem, Shl, Sub,
-                      Switch, UDiv, URem, Unreachable, Xor, br, cond_br, ret, ret_void};
+                      Switch, UDiv, URem, Unreachable, Xor};
 }
