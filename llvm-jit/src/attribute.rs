569816a3a6542b9835824f5f5694687b13af1b4a
Flier Lu <flier.lu@gmail.com>
2017-08-03 17:02:03 +0800

    add attribute

---
+++ llvm-jit/src/attribute.rs
@@ -0,0 +1,411 @@
+#![allow(non_upper_case_globals)]
+
+use std::borrow::Cow;
+use std::ptr;
+
+use llvm::*;
+use llvm::core::*;
+use llvm::prelude::*;
+
+use context::Context;
+use function::Function;
+use insts::CallSite;
+use utils::{AsBool, from_unchecked_cstr, unchecked_cstring};
+
+pub type AttributeIndex = LLVMAttributeIndex;
+
+pub const RETURN_INDEX: AttributeIndex = LLVMAttributeReturnIndex;
+pub const FUNCTION_INDEX: AttributeIndex = LLVMAttributeFunctionIndex;
+
+pub mod attrs {
+    //! The target-independent enum attributes.
+
+    use super::AttributeKind;
+
+    lazy_static! {
+        /// Alignment of parameter (5 bits) stored as log2 of alignment with +1 bias.
+        /// 0 means unaligned (different from align(1)).
+        pub static ref Alignment: AttributeKind = AttributeKind::get("align").unwrap();
+
+        /// The result of the function is guaranteed to point to a number of bytes that
+        /// we can determine if we know the value of the function's arguments.
+        pub static ref AllocSize: AttributeKind = AttributeKind::get("allocsize").unwrap();
+
+        /// inline=always.
+        pub static ref AlwaysInline: AttributeKind = AttributeKind::get("alwaysinline").unwrap();
+
+        /// Function can access memory only using pointers based on its arguments.
+        pub static ref ArgMemOnly: AttributeKind = AttributeKind::get("argmemonly").unwrap();
+
+        /// Callee is recognized as a builtin, despite nobuiltin attribute on its declaration.
+        pub static ref Builtin: AttributeKind = AttributeKind::get("builtin").unwrap();
+
+        /// Pass structure by value.
+        pub static ref ByVal: AttributeKind = AttributeKind::get("byval").unwrap();
+
+        /// Marks function as being in a cold path.
+        pub static ref Cold: AttributeKind = AttributeKind::get("cold").unwrap();
+
+        /// Can only be moved to control-equivalent blocks.
+        pub static ref Convergent: AttributeKind = AttributeKind::get("convergent").unwrap();
+
+        /// Pointer is known to be dereferenceable.
+        pub static ref Dereferenceable: AttributeKind = AttributeKind::get("dereferenceable").unwrap();
+
+        /// Pointer is either null or dereferenceable.
+        pub static ref DereferenceableOrNull: AttributeKind = AttributeKind::get("dereferenceable_or_null").unwrap();
+
+        /// Function may only access memory that is inaccessible from IR.
+        pub static ref InaccessibleMemOnly: AttributeKind = AttributeKind::get("inaccessiblememonly").unwrap();
+
+        /// Function may only access memory that is either inaccessible from the IR,
+        /// or pointed to by its pointer arguments.
+        pub static ref InaccessibleMemOrArgMemOnly: AttributeKind = AttributeKind::get("inaccessiblemem_or_argmemonly").unwrap();
+
+        /// Pass structure in an alloca.
+        pub static ref InAlloca: AttributeKind = AttributeKind::get("inalloca").unwrap();
+
+        /// Source said inlining was desirable.
+        pub static ref InlineHint: AttributeKind = AttributeKind::get("inlinehint").unwrap();
+
+        /// Force argument to be passed in register.
+        pub static ref InReg: AttributeKind = AttributeKind::get("inreg").unwrap();
+
+        /// Build jump-instruction tables and replace refs.
+        pub static ref JumpTable: AttributeKind = AttributeKind::get("jumptable").unwrap();
+
+        /// Function must be optimized for size first.
+        pub static ref MinSize: AttributeKind = AttributeKind::get("minsize").unwrap();
+
+        /// Naked function.
+        pub static ref Naked: AttributeKind = AttributeKind::get("naked").unwrap();
+
+        /// Nested function static chain.
+        pub static ref Nest: AttributeKind = AttributeKind::get("nest").unwrap();
+
+        /// Considered to not alias after call.
+        pub static ref NoAlias: AttributeKind = AttributeKind::get("noalias").unwrap();
+
+        /// Callee isn't recognized as a builtin.
+        pub static ref NoBuiltin: AttributeKind = AttributeKind::get("nobuiltin").unwrap();
+
+        /// Function creates no aliases of pointer.
+        pub static ref NoCapture: AttributeKind = AttributeKind::get("nocapture").unwrap();
+
+        /// Call cannot be duplicated.
+        pub static ref NoDuplicate: AttributeKind = AttributeKind::get("noduplicate").unwrap();
+
+        /// Disable implicit floating point insts.
+        pub static ref NoImplicitFloat: AttributeKind = AttributeKind::get("noimplicitfloat").unwrap();
+
+        /// inline=never.
+        pub static ref NoInline: AttributeKind = AttributeKind::get("noinline").unwrap();
+
+        /// Function is called early and/or often, so lazy binding isn't worthwhile.
+        pub static ref NonLazyBind: AttributeKind = AttributeKind::get("nonlazybind").unwrap();
+
+        /// Pointer is known to be not null.
+        pub static ref NonNull: AttributeKind = AttributeKind::get("nonnull").unwrap();
+
+        /// The function does not recurse.
+        pub static ref NoRecurse: AttributeKind = AttributeKind::get("norecurse").unwrap();
+
+        /// Disable redzone.
+        pub static ref NoRedZone: AttributeKind = AttributeKind::get("noredzone").unwrap();
+
+        /// Mark the function as not returning.
+        pub static ref NoReturn: AttributeKind = AttributeKind::get("noreturn").unwrap();
+
+        /// Function doesn't unwind stack.
+        pub static ref NoUnwind: AttributeKind = AttributeKind::get("nounwind").unwrap();
+
+        /// opt_size.
+        pub static ref OptimizeForSize: AttributeKind = AttributeKind::get("optsize").unwrap();
+
+        /// Function must not be optimized.
+        pub static ref OptimizeNone: AttributeKind = AttributeKind::get("optnone").unwrap();
+
+        /// Function does not access memory.
+        pub static ref ReadNone: AttributeKind = AttributeKind::get("readnone").unwrap();
+
+        /// Function only reads from memory.
+        pub static ref ReadOnly: AttributeKind = AttributeKind::get("readonly").unwrap();
+
+        /// Return value is always equal to this argument.
+        pub static ref Returned: AttributeKind = AttributeKind::get("returned").unwrap();
+
+        /// Function can return twice.
+        pub static ref ReturnsTwice: AttributeKind = AttributeKind::get("returns_twice").unwrap();
+
+        /// Safe Stack protection.
+        pub static ref SafeStack: AttributeKind = AttributeKind::get("safestack").unwrap();
+
+        /// Sign extended before/after call.
+        pub static ref SExt: AttributeKind = AttributeKind::get("signext").unwrap();
+
+        /// Alignment of stack for function (3 bits)  stored as log2 of alignment with
+        /// +1 bias 0 means unaligned (different from alignstack=(1)).
+        pub static ref StackAlignment: AttributeKind = AttributeKind::get("alignstack").unwrap();
+
+        /// Function can be speculated.
+        pub static ref Speculatable: AttributeKind = AttributeKind::get("speculatable").unwrap();
+
+        /// Stack protection.
+        pub static ref StackProtect: AttributeKind = AttributeKind::get("ssp").unwrap();
+
+        /// Stack protection required.
+        pub static ref StackProtectReq: AttributeKind = AttributeKind::get("sspreq").unwrap();
+
+        /// Strong Stack protection.
+        pub static ref StackProtectStrong: AttributeKind = AttributeKind::get("sspstrong").unwrap();
+
+        /// Hidden pointer to structure to return.
+        pub static ref StructRet: AttributeKind = AttributeKind::get("sret").unwrap();
+
+        /// AddressSanitizer is on.
+        pub static ref SanitizeAddress: AttributeKind = AttributeKind::get("sanitize_address").unwrap();
+
+        /// ThreadSanitizer is on.
+        pub static ref SanitizeThread: AttributeKind = AttributeKind::get("sanitize_thread").unwrap();
+
+        /// MemorySanitizer is on.
+        pub static ref SanitizeMemory: AttributeKind = AttributeKind::get("sanitize_memory").unwrap();
+
+        /// Argument is swift error.
+        pub static ref SwiftError: AttributeKind = AttributeKind::get("swifterror").unwrap();
+
+        /// Argument is swift self/context.
+        pub static ref SwiftSelf: AttributeKind = AttributeKind::get("swiftself").unwrap();
+
+        /// Function must be in a unwind table.
+        pub static ref UWTable: AttributeKind = AttributeKind::get("uwtable").unwrap();
+
+        /// Function only writes to memory.
+        pub static ref WriteOnly: AttributeKind = AttributeKind::get("writeonly").unwrap();
+
+        /// Zero extended before/after call.
+        pub static ref ZExt: AttributeKind = AttributeKind::get("zeroext").unwrap();
+            }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct AttributeKind(u32);
+
+inherit_from!(AttributeKind, u32);
+
+impl AttributeKind {
+    /// Return an unique id given the name of a enum attribute.
+    pub fn get<S: AsRef<str>>(name: S) -> Option<Self> {
+        let name = name.as_ref();
+
+        let kind =
+            unsafe { LLVMGetEnumAttributeKindForName(name.as_ptr() as *const i8, name.len()) };
+
+        if kind == 0 { None } else { Some(kind.into()) }
+    }
+
+    pub fn last() -> Self {
+        unsafe { LLVMGetLastEnumAttributeKind() }.into()
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Attribute(LLVMAttributeRef);
+
+inherit_from!(Attribute, LLVMAttributeRef);
+
+impl Attribute {
+    pub fn is_enum_attribute(&self) -> bool {
+        unsafe { LLVMIsEnumAttribute(self.as_raw()).as_bool() }
+    }
+
+    pub fn is_string_attribute(&self) -> bool {
+        unsafe { LLVMIsStringAttribute(self.as_raw()).as_bool() }
+    }
+}
+
+pub struct EnumAttribute(Attribute);
+
+inherit_from!(EnumAttribute, Attribute, LLVMAttributeRef);
+
+impl EnumAttribute {
+    /// Get the unique id corresponding to the enum attribute passed as argument.
+    pub fn kind(&self) -> AttributeKind {
+        unsafe { LLVMGetEnumAttributeKind(self.as_raw()) }.into()
+    }
+
+    /// Get the enum attribute's value.
+    pub fn value(&self) -> u64 {
+        unsafe { LLVMGetEnumAttributeValue(self.as_raw()) }
+    }
+}
+
+pub struct StringAttribute(Attribute);
+
+inherit_from!(StringAttribute, Attribute, LLVMAttributeRef);
+
+impl StringAttribute {
+    /// Get the string attribute's kind.
+    pub fn kind(&self) -> Cow<str> {
+        let mut len = 0;
+        let p = unsafe { LLVMGetStringAttributeKind(self.as_raw(), &mut len) };
+
+        from_unchecked_cstr(p as *const u8, len as usize)
+    }
+
+    /// Get the string attribute's value.
+    pub fn value(&self) -> Cow<str> {
+        let mut len = 0;
+        let p = unsafe { LLVMGetStringAttributeValue(self.as_raw(), &mut len) };
+
+        from_unchecked_cstr(p as *const u8, len as usize)
+    }
+}
+
+pub trait AttributeGroups {
+    /// adds the attribute to the list of attributes.
+    fn add_attribute(&self, idx: AttributeIndex, attr: Attribute);
+
+    /// get the attribute from the list of attributes.
+    fn get_attributes(&self, idx: AttributeIndex) -> Vec<Attribute>;
+
+    /// get the attribute from the list of attributes.
+    fn get_enum_attribute(&self, idx: AttributeIndex, kind: AttributeKind) -> EnumAttribute;
+
+    /// get the attribute from the list of attributes.
+    fn get_string_attribute(&self, idx: AttributeIndex, kind: &str) -> StringAttribute;
+
+    /// removes the attribute from the list of attributes.
+    fn remove_enum_attribute(&self, idx: AttributeIndex, kind: AttributeKind);
+
+    /// removes the attribute from the list of attributes.
+    fn remove_string_attribute(&self, idx: AttributeIndex, kind: &str);
+}
+
+impl Context {
+    /// Create an enum attribute.
+    pub fn create_enum_attribute(&self, kind: AttributeKind, value: u64) -> EnumAttribute {
+        unsafe { LLVMCreateEnumAttribute(self.as_raw(), kind.as_raw(), value) }.into()
+    }
+
+    /// Create a string attribute.
+    pub fn create_string_attribute(&self, kind: &str, value: &str) -> StringAttribute {
+        unsafe {
+            LLVMCreateStringAttribute(
+                self.as_raw(),
+                kind.as_ptr() as *const i8,
+                kind.len() as u32,
+                value.as_ptr() as *const i8,
+                value.len() as u32,
+            )
+        }.into()
+    }
+}
+
+impl Function {
+    /// Add a target-dependent attribute to a function
+    pub fn add_target_dependent_attribute(&self, kind: &str, value: &str) {
+        unsafe {
+            LLVMAddTargetDependentFunctionAttr(
+                self.as_raw(),
+                unchecked_cstring(kind).as_ptr(),
+                unchecked_cstring(value).as_ptr(),
+            )
+        }
+    }
+}
+
+impl AttributeGroups for Function {
+    fn add_attribute(&self, idx: AttributeIndex, attr: Attribute) {
+        unsafe { LLVMAddAttributeAtIndex(self.as_raw(), idx, attr.as_raw()) }
+    }
+
+    fn get_attributes(&self, idx: AttributeIndex) -> Vec<Attribute> {
+        let count = unsafe { LLVMGetAttributeCountAtIndex(self.as_raw(), idx) };
+        let mut attrs: Vec<LLVMAttributeRef> = vec![ptr::null_mut(); count as usize];
+
+        unsafe { LLVMGetAttributesAtIndex(self.as_raw(), idx, attrs.as_mut_ptr()) };
+
+        attrs.into_iter().map(Attribute::from_raw).collect()
+    }
+
+    fn get_enum_attribute(&self, idx: AttributeIndex, kind: AttributeKind) -> EnumAttribute {
+        unsafe { LLVMGetEnumAttributeAtIndex(self.as_raw(), idx, kind.as_raw()) }.into()
+    }
+
+    fn get_string_attribute(&self, idx: AttributeIndex, kind: &str) -> StringAttribute {
+        unsafe {
+            LLVMGetStringAttributeAtIndex(
+                self.as_raw(),
+                idx,
+                kind.as_ptr() as *const i8,
+                kind.len() as u32,
+            )
+        }.into()
+    }
+
+    fn remove_enum_attribute(&self, idx: AttributeIndex, kind: AttributeKind) {
+        unsafe { LLVMRemoveEnumAttributeAtIndex(self.as_raw(), idx, kind.as_raw()) }
+    }
+
+    fn remove_string_attribute(&self, idx: AttributeIndex, kind: &str) {
+        unsafe {
+            LLVMRemoveStringAttributeAtIndex(
+                self.as_raw(),
+                idx,
+                kind.as_ptr() as *const i8,
+                kind.len() as u32,
+            )
+        }
+    }
+}
+
+impl<T: CallSite> AttributeGroups for T {
+    fn add_attribute(&self, idx: AttributeIndex, attr: Attribute) {
+        unsafe { LLVMAddCallSiteAttribute(self.as_raw(), idx, attr.as_raw()) }
+    }
+
+    fn get_attributes(&self, idx: AttributeIndex) -> Vec<Attribute> {
+        let count = unsafe { LLVMGetCallSiteAttributeCount(self.as_raw(), idx) };
+        let mut attrs: Vec<LLVMAttributeRef> = vec![ptr::null_mut(); count as usize];
+
+        unsafe { LLVMGetCallSiteAttributes(self.as_raw(), idx, attrs.as_mut_ptr()) };
+
+        attrs.into_iter().map(Attribute::from_raw).collect()
+    }
+
+    fn get_enum_attribute(&self, idx: AttributeIndex, kind: AttributeKind) -> EnumAttribute {
+        unsafe { LLVMGetCallSiteEnumAttribute(self.as_raw(), idx, kind.as_raw()) }.into()
+    }
+
+    fn get_string_attribute(&self, idx: AttributeIndex, kind: &str) -> StringAttribute {
+        unsafe {
+            LLVMGetCallSiteStringAttribute(
+                self.as_raw(),
+                idx,
+                kind.as_ptr() as *const i8,
+                kind.len() as u32,
+            )
+        }.into()
+    }
+
+    fn remove_enum_attribute(&self, idx: AttributeIndex, kind: AttributeKind) {
+        unsafe { LLVMRemoveCallSiteEnumAttribute(self.as_raw(), idx, kind.as_raw()) }
+    }
+
+    fn remove_string_attribute(&self, idx: AttributeIndex, kind: &str) {
+        unsafe {
+            LLVMRemoveCallSiteStringAttribute(
+                self.as_raw(),
+                idx,
+                kind.as_ptr() as *const i8,
+                kind.len() as u32,
+            )
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
--- llvm-jit/src/lib.rs
+++ llvm-jit/src/lib.rs
@@ -1,3 +1,5 @@
+#![recursion_limit="128"]
+
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy(conf_file=".clippy.toml")))]
 #![cfg_attr(feature="clippy", allow(module_inception, block_in_if_condition_stmt))]
@@ -6,6 +8,8 @@
 extern crate log;
 #[macro_use]
 extern crate error_chain;
+#[macro_use]
+extern crate lazy_static;
 #[macro_use]
 extern crate bitflags;
 extern crate hexplay;
@@ -25,6 +29,7 @@ extern crate matches;
 #[macro_use]
 extern crate hamcrest;
 
+#[doc(hidden)]
 #[macro_use]
 pub mod errors;
 #[macro_use]
@@ -37,6 +42,7 @@ mod types;
 mod value;
 #[macro_use]
 mod constant;
+mod attribute;
 mod global;
 mod function;
 #[macro_use]
@@ -51,6 +57,9 @@ mod membuf;
 mod object;
 mod bitcode;
 
+pub use attribute::attrs;
+
+pub use attribute::{Attribute, EnumAttribute, StringAttribute};
 pub use constant::{Constant, ConstantArray, ConstantFP, ConstantInt, ConstantString,
                    ConstantStruct, ConstantVector};
 pub use context::{Context, GlobalContext};
@@ -66,10 +75,18 @@ pub use object::{ObjectFile, Section, SectionIter, Symbol, SymbolIter};
 pub use passmgr::{Pass, PassManager, PassManagerBuilder, PassRegistry};
 pub use types::{ArrayType, FloatingPointType, IntegerType, OtherType, PointerType, StructType,
                 TypeKind, TypeRef, VectorType};
-pub use utils::{AsBool, AsLLVMBool, AsResult, Boolinator};
 pub use value::{BlockAddress, Instruction, ValueKind, ValueRef};
 
 pub mod prelude {
+    //! A prelude for writing LLVM JIT code.
+
+    pub use context::Context;
+    pub use engine::{ExecutionEngine, GenericValue};
+    pub use function::{Function, FunctionType};
+    pub use insts::{IRBuilder, Position};
+    pub use module::Module;
+
+    pub use attribute::AttributeGroups;
     pub use constant::{ConstantFPs, ConstantInts, ConstantStrings, Constants, ToConstantArray,
                        ToConstantStruct};
     pub use insts::InstructionBuilder;
