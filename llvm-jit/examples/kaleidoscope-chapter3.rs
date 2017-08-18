7efb2772d97633114e5e75b4d885d948d7bbce41
Flier Lu <flier.lu@gmail.com>
2017-08-18 17:18:48 +0800

    generate IR code

--- llvm-jit/examples/kaleidoscope-chapter3.rs
+++ llvm-jit/examples/kaleidoscope-chapter3.rs
@@ -8,11 +8,17 @@ extern crate rustyline;
 extern crate llvm_jit as jit;
 
 mod errors {
+    use jit;
+
     use lexer::Token;
 
     error_chain!{
         foreign_links {
 
+        }
+
+        links {
+            Jit(jit::errors::Error, jit::errors::ErrorKind);
         }
 
         errors {
@@ -200,10 +206,10 @@ mod lexer {
 mod ast {
     use std::fmt;
 
-    use jit::insts::*;
     use jit::prelude::*;
 
-    use errors::{ErrorKind, Result};
+    use codegen::CodeGenerator;
+    use errors::Result;
     use lexer::Token;
 
     #[derive(Debug, PartialEq, PartialOrd)]
@@ -228,7 +234,7 @@ mod ast {
 
     /// Expr - Base class for all expression nodes.
     pub trait Expr: fmt::Debug {
-        fn codegen(&self, module: &Module, builder: &IRBuilder) -> Result<ValueRef>;
+        fn codegen(&self, gen: &mut CodeGenerator) -> Result<ValueRef>;
     }
 
     /// NumberExpr - Expression class for numeric literals like "1.0".
@@ -237,25 +243,10 @@ mod ast {
         pub val: f64,
     }
 
-    impl Expr for NumberExpr {
-        fn codegen(&self, module: &Module, _builder: &IRBuilder) -> Result<ValueRef> {
-            Ok(module.context().double_t().real(self.val).into())
-        }
-    }
-
     /// VariableExpr - Expression class for referencing a variable, like "a".
     #[derive(Debug)]
     pub struct VariableExpr {
         pub name: String,
-    }
-
-    impl Expr for VariableExpr {
-        fn codegen(&self, module: &Module, _builder: &IRBuilder) -> Result<ValueRef> {
-            module
-                .get_global_var(&self.name)
-                .map(|var| var.into())
-                .ok_or_else(|| ErrorKind::UnknownVariable(self.name.clone()).into())
-        }
     }
 
     /// BinaryExpr - Expression class for a binary operator.
@@ -266,55 +257,11 @@ mod ast {
         pub rhs: Box<Expr>,
     }
 
-    impl Expr for BinaryExpr {
-        fn codegen(&self, module: &Module, builder: &IRBuilder) -> Result<ValueRef> {
-            let context = module.context();
-            let f64_t = context.double_t();
-
-            let lhs = self.lhs.codegen(module, builder)?;
-            let rhs = self.rhs.codegen(module, builder)?;
-
-            Ok(
-                match self.op {
-                    BinOp::Add => add!(lhs, rhs; "addtmp").emit_to(builder),
-                    BinOp::Sub => sub!(lhs, rhs; "subtmp").emit_to(builder),
-                    BinOp::Mul => mul!(lhs, rhs; "multmp").emit_to(builder),
-                    BinOp::LessThen => {
-                        uitofp!(fcmp!(ult lhs, rhs; "cmptmp"), f64_t; "booltmp").emit_to(builder)
-                    }
-                }.into(),
-            )
-        }
-    }
-
     /// CallExpr - Expression class for function calls.
     #[derive(Debug)]
     pub struct CallExpr {
         pub callee: String,
         pub args: Vec<Box<Expr>>,
-    }
-
-    impl Expr for CallExpr {
-        fn codegen(&self, module: &Module, builder: &IRBuilder) -> Result<ValueRef> {
-            if let Some(func) = module.get_function(&self.callee) {
-                if self.args.len() != func.param_count() {
-                    bail!(ErrorKind::IncorrectArguments(
-                        self.args.len(),
-                        func.param_count(),
-                    ))
-                }
-
-                let mut args = vec![];
-
-                for arg in &self.args {
-                    args.push(arg.codegen(module, builder)?);
-                }
-
-                Ok(call(func, args, "calltmp").emit_to(builder).into())
-            } else {
-                bail!(ErrorKind::UnknownFunction(self.callee.clone()))
-            }
-        }
     }
 
     /// Prototype - This class represents the "prototype" for a function,
@@ -326,23 +273,11 @@ mod ast {
         pub args: Vec<String>,
     }
 
-    impl Expr for Prototype {
-        fn codegen(&self, module: &Module, builder: &IRBuilder) -> Result<ValueRef> {
-            unimplemented!()
-        }
-    }
-
     /// FunctionAST - This class represents a function definition itself.
     #[derive(Debug)]
     pub struct Function {
         pub proto: Prototype,
         pub body: Box<Expr>,
-    }
-
-    impl Expr for Function {
-        fn codegen(&self, module: &Module, builder: &IRBuilder) -> Result<ValueRef> {
-            unimplemented!()
-        }
     }
 }
 
@@ -646,6 +581,149 @@ mod parser {
     }
 }
 
+//===----------------------------------------------------------------------===//
+// Code Generation
+//===----------------------------------------------------------------------===//
+
+mod codegen {
+    use std::collections::HashMap;
+
+    use jit::insts::*;
+    use jit::prelude::*;
+
+    use ast;
+    use errors::{ErrorKind, Result};
+
+    pub struct CodeGenerator {
+        pub context: Context,
+        pub module: Module,
+        pub builder: IRBuilder,
+        pub named_values: HashMap<String, ValueRef>,
+    }
+
+    pub fn new(name: &str) -> CodeGenerator {
+        let context = Context::new();
+        let module = context.create_module(name);
+        let builder = context.create_builder();
+
+        CodeGenerator {
+            context,
+            module,
+            builder,
+            named_values: HashMap::new(),
+        }
+    }
+
+    impl ast::Expr for ast::NumberExpr {
+        fn codegen(&self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+            Ok(gen.context.double_t().real(self.val).into())
+        }
+    }
+
+    impl ast::Expr for ast::VariableExpr {
+        fn codegen(&self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+            gen.named_values.get(&self.name).map(|v| *v).ok_or_else(
+                || {
+                    ErrorKind::UnknownVariable(self.name.clone()).into()
+                },
+            )
+        }
+    }
+
+    impl ast::Expr for ast::BinaryExpr {
+        fn codegen(&self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+            let f64_t = gen.context.double_t();
+
+            let lhs = self.lhs.codegen(gen)?;
+            let rhs = self.rhs.codegen(gen)?;
+
+            Ok(
+                match self.op {
+                    ast::BinOp::Add => add!(lhs, rhs; "addtmp").emit_to(&gen.builder),
+                    ast::BinOp::Sub => sub!(lhs, rhs; "subtmp").emit_to(&gen.builder),
+                    ast::BinOp::Mul => mul!(lhs, rhs; "multmp").emit_to(&gen.builder),
+                    ast::BinOp::LessThen => {
+                        uitofp!(fcmp!(ult lhs, rhs; "cmptmp"), f64_t; "booltmp")
+                            .emit_to(&gen.builder)
+                    }
+                }.into(),
+            )
+        }
+    }
+
+    impl ast::Expr for ast::CallExpr {
+        fn codegen(&self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+            if let Some(func) = gen.module.get_function(&self.callee) {
+                if self.args.len() != func.param_count() {
+                    bail!(ErrorKind::IncorrectArguments(
+                        self.args.len(),
+                        func.param_count(),
+                    ))
+                }
+
+                let mut args = vec![];
+
+                for arg in &self.args {
+                    args.push(arg.codegen(gen)?);
+                }
+
+                Ok(call(func, args, "calltmp").emit_to(&gen.builder).into())
+            } else {
+                bail!(ErrorKind::UnknownFunction(self.callee.clone()))
+            }
+        }
+    }
+
+    impl ast::Expr for ast::Prototype {
+        fn codegen(&self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+            let f64_t = gen.context.double_t();
+            let doubles = vec![f64_t; self.args.len()];
+            let func_t = FunctionType::new(f64_t, &doubles, false);
+            let func = gen.module.add_function(&self.name, func_t);
+
+            for (name, arg) in self.args.iter().zip(func.params()) {
+                arg.set_name(name);
+            }
+
+            Ok(func.into())
+        }
+    }
+
+    impl ast::Expr for ast::Function {
+        fn codegen(&self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+            if let Some(func) = gen.module.get_function(&self.proto.name) {
+                func.delete();
+            }
+
+            let func: Function = self.proto.codegen(gen)?.into();
+
+            // Create a new basic block to start insertion into.
+            let bb = func.append_basic_block_in_context("entry", &gen.context);
+            gen.builder.position_at_end(bb);
+
+            // Record the function arguments in the NamedValues map.
+            gen.named_values = func.params()
+                .map(|arg| (String::from(arg.name().unwrap()), arg))
+                .collect::<HashMap<String, ValueRef>>();
+
+            trace!("{} params: {:?}", gen.named_values.len(), gen.named_values);
+
+            let ret_val = self.body.codegen(gen)?;
+
+            ret!(ret_val).emit_to(&gen.builder);
+
+            //func.verify()?;
+
+            Ok(func.into())
+        }
+    }
+}
+
+use jit::prelude::*;
+
+use ast::Expr;
+use codegen::CodeGenerator;
+use errors::Result;
 use lexer::Token;
 
 //===----------------------------------------------------------------------===//
@@ -656,38 +734,59 @@ impl<I> parser::Parser<I>
 where
     I: Iterator<Item = char>,
 {
-    fn handle_definition(&mut self) {
-        match self.parse_definition() {
-            Ok(func) => println!("parsed a function difinition: {:?}", func),
-            Err(err) => {
-                let token = self.next_token();
+    fn handle_definition(&mut self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+        self.parse_definition().and_then(|func| {
+            println!("parsed a function difinition: {:?}", func);
 
-                warn!("skip token {:?} for error recovery, {}", token, err)
-            }
-        }
+            let ir = func.codegen(gen)?;
+
+            println!("read function definition:\n{}", ir);
+
+            Ok(ir)
+        })
     }
 
-    fn handle_extern(&mut self) {
-        match self.parse_extern() {
-            Ok(proto) => println!("parsed a extern prototype: {:?}", proto),
-            Err(err) => {
-                let token = self.next_token();
+    fn handle_extern(&mut self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+        self.parse_extern().and_then(|proto| {
+            println!("parsed a extern prototype: {:?}", proto);
 
-                warn!("skip token {:?} for error recovery, {}", token, err)
-            }
-        }
+            let ir = proto.codegen(gen)?;
+
+            println!("read extern:\n{}", ir);
+
+            Ok(ir)
+        })
     }
 
-    fn handle_top_level_expression(&mut self) {
-        match self.parse_top_level_expr() {
-            Ok(func) => println!("parsed a top-level expr: {:?}", func),
-            Err(err) => {
-                let token = self.next_token();
+    fn handle_top_level_expression(&mut self, gen: &mut CodeGenerator) -> Result<ValueRef> {
+        self.parse_top_level_expr().and_then(|func| {
+            println!("parsed a top-level expr: {:?}", func);
 
-                warn!("skip token {:?} for error recovery, {}", token, err)
-            }
+            let ir = func.codegen(gen)?;
+
+            println!("read top-level expression:\n{}", ir);
+
+            Ok(ir)
+        })
+    }
+
+    fn handle_top(&mut self, gen: &mut CodeGenerator) -> Result<Parsed> {
+        // top ::= definition | external | expression | ';'
+        match self.next_token().clone() {
+            Token::Def => Ok(Parsed::Code(self.handle_definition(gen)?)),
+            Token::Extern => Ok(Parsed::Code(self.handle_extern(gen)?)),
+            Token::Eof => Ok(Parsed::ToEnd),
+            token @ Token::Character(';') |
+            token @ Token::Comment(_) => Ok(Parsed::Skipped(token)),
+            _ => Ok(Parsed::Code(self.handle_top_level_expression(gen)?)),
         }
     }
+}
+
+enum Parsed {
+    ToEnd,
+    Code(ValueRef),
+    Skipped(Token),
 }
 
 struct Lines<'a> {
@@ -761,21 +860,31 @@ impl<'a> Iterator for Lines<'a> {
 fn main() {
     pretty_env_logger::init().unwrap();
 
+    // Make the module, which holds all the code.
+    let mut gen = codegen::new("my cool jit");
+
     for code in Lines::new() {
         debug!("parsing code: {}", code);
 
         let mut parser = parser::new(code.chars());
 
+        // Run the main "interpreter loop" now.
         loop {
-            // top ::= definition | external | expression | ';'
-            match parser.next_token().clone() {
-                Token::Def => parser.handle_definition(),
-                Token::Extern => parser.handle_extern(),
-                Token::Eof => break,
-                Token::Character(';') => trace!("ignore top-level semicolons."),
-                Token::Comment(ref comment) => trace!("ignore comment: {}", comment),
-                _ => parser.handle_top_level_expression(),
+            match parser.handle_top(&mut gen) {
+                Ok(Parsed::ToEnd) => {
+                    break;
+                }
+                Ok(Parsed::Skipped(token)) => trace!("skipped {:?}", token),
+                Ok(_) => {}
+                Err(err) => {
+                    let token = parser.next_token();
+
+                    println!("skip token {:?} for error recovery, {}", token, err);
+                }
             }
         }
     }
+
+    // Print out all of the generated code.
+    println!("module:\n{}", gen.module);
 }
