f06c34c78a04ace580d30c0d8c8b1cfdaf445e31
Flier Lu <flier.lu@gmail.com>
2018-03-13 16:45:59 +0800

    pass wasm module

--- llvm-wasm/examples/wavm.rs
+++ llvm-wasm/examples/wavm.rs
@@ -4,8 +4,10 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate failure;
 extern crate getopts;
+extern crate parity_wasm;
 
 use std::env;
+use std::process;
 
 use failure::Error;
 use getopts::{Matches, Options};
@@ -13,19 +15,26 @@ use getopts::{Matches, Options};
 fn parse_cmdline(program: &str, args: &[String]) -> Result<Option<Matches>, Error> {
     let mut opts = Options::new();
 
-     opts.optflag("h", "help", "print this help menu");
+    opts.optopt(
+        "f",
+        "function",
+        "main",
+        "function name to run in module rather than main",
+    );
+    opts.optflag("c", "check", "exit after checking that the program is valid");
+    opts.optflag("h", "help", "print this help menu");
 
     let matches = opts.parse(args)?;
 
     if matches.opt_present("h") {
-        let brief = format!("Usage: {} FILE [options]", program);
+        let brief = format!("Usage: {} [options] FILE", program);
 
         print!("{}", opts.usage(&brief));
 
         Ok(None)
     } else {
         Ok(Some(matches))
-}
+    }
 }
 
 fn main() {
@@ -34,10 +43,42 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let (program, args) = args.split_first().unwrap();
 
-    match parse_cmdline(program, args) {
-        Ok(Some(opts)) => {
+    let matches = match parse_cmdline(program, args) {
+        Ok(Some(matches)) => matches,
+        Ok(None) => {
+            process::exit(0);
         }
-        Ok(None) => {}
-        Err(err) => eprintln!("Fail to parse command line, {}", err),
+        Err(err) => {
+            eprintln!("Fail to parse command line, {}", err);
+            process::exit(-1);
+        }
+    };
+
+    let (program, args) = match matches.free.as_slice().split_first() {
+        Some((program, args)) => {
+            info!("loading module from {}", program);
+
+            (program, args)
+        }
+        None => {
+            eprintln!("missed program file (.wast | .wasm)");
+            process::exit(-2);
+        }
+    };
+
+    let module = match parity_wasm::deserialize_file(program) {
+        Ok(module) => {
+            debug!("loaded module: {:?}", module);
+
+            module
+        }
+        Err(err) => {
+            eprintln!("fail to load module, {}", err);
+            process::exit(-3);
+        }
+    };
+
+    if matches.opt_present("check") {
+        process::exit(0);
     }
 }
